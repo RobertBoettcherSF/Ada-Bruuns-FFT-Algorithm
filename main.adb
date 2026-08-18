e96aa81bcae9421c3db61d4cb040a6d41f175f5c
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-18 19:17:46 +0200

    Initialize main.adb with project information

---
+++ main.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line("Bruun's FFT Algorithm Project Loaded.");
+   Put_Line("Codebase is configured for critical systems testing.");
+   Put_Line("Please run 'make test' to execute the V&V verification suite.");
+end Main;
